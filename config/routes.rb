f0d3497cfe2a4bda81a05149313bd55be03fa690
WowanYs <wowanys00@gmail.com>
2019-02-06 00:00:13 +0200

    add controller and model songs

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'songs/new'
+  get 'songs/edit'
+  get 'songs/show'
+  get 'songs/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
